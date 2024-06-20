4ebf7b9b98d80bbe12fabc4193da4975a97d1f75
EdgarTI1001 <edgarmarques784@gmail.com>
2024-06-19 20:09:21 -0400

    first commit

---
+++ pad-m/src/main/java/padm/io/pad_m/PadMApplication.java
@@ -0,0 +1,13 @@
+package padm.io.pad_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadMApplication.class, args);
+	}
+
+}
