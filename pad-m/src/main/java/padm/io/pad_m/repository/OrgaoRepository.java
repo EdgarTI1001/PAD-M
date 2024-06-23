410a4b07e6ca7796a8c7c7cf472cf1b7745aed81
beeth <beeth@DESKTOP-5990OI3>
2024-06-22 21:51:49 -0400

    Implementacao de conexao com banco e REpo, Controler e service de Orgao

---
+++ pad-m/src/main/java/padm/io/pad_m/repository/OrgaoRepository.java
@@ -0,0 +1,11 @@
+package padm.io.pad_m.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import padm.io.pad_m.domain.Orgao;
+
+public interface OrgaoRepository extends JpaRepository<Orgao, Integer> {
+	
+	
+	
+}
