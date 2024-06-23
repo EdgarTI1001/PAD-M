c3a97b081ce79fd62454fc328660dac7111ed351
EdgarTI1001 <edgarmarques784@gmail.com>
2024-06-22 20:15:35 -0400

    - domain, banner, classe usuário

---
+++ pad-m/src/main/java/padm/io/pad_m/domain/Usuario.java
@@ -0,0 +1,63 @@
+package padm.io.pad_m.domain;
+
+/*import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Usuario {
+
+	private Integer id;
+	private String name;
+	private Long cpf;
+	private String nickName;
+	private Servidor servidor;
+
+	@JsonIgnore
+	private String senha;
+
+	private int perfil;
+
+	private UnidadeLotacao lotacao;
+
+	private int ativo;
+
+	private String certificado;
+
+	@Column(name = "dataExpiracao")
+	private DateTime dataExpiracao;
+
+	@Column(name = "ultimoAcesso")
+	private DateTime ultimoAcesso;
+
+	@Column(name = "dataCriacao")
+	private DateTime dataCriacao;
+
+	private String image;
+
+	private int flag;
+
+	public Usuario() {
+	}
+
+	public Usuario(Integer id, String name, Long cpf, String nickName, Servidor servidor, String senha, int perfil,
+			UnidadeLotacao lotacao, int ativo, String certificado, DateTime dataExpiracao, DateTime ultimoAcesso,
+			DateTime dataCriacao, String image, int flag) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.nickName = nickName;
+		this.servidor = servidor;
+		this.senha = senha;
+		this.perfil = perfil;
+		this.lotacao = lotacao;
+		this.ativo = ativo;
+		this.certificado = certificado;
+		this.dataExpiracao = dataExpiracao;
+		this.ultimoAcesso = ultimoAcesso;
+		this.dataCriacao = dataCriacao;
+		this.image = image;
+		this.flag = flag;
+	}
+	*/
+	
+	
+
+
