0e0d36955f69b5bc50944eaf34b3f62100baaf09
EdgarTI1001 <edgarmarques784@gmail.com>
2024-07-18 20:21:34 -0400

    - ajuste cad usuario

--- pad-m/src/main/java/padm/io/pad_m/controller/UsuarioController.java
+++ pad-m/src/main/java/padm/io/pad_m/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package padm.io.pad_m.controller;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import padm.io.pad_m.domain.Usuario;
-import padm.io.pad_m.enums.Perfil;
 import padm.io.pad_m.service.UsuarioService;
-
 
 @Controller
 @RequestMapping("/usuarios")
@@ -24,69 +23,46 @@ public class UsuarioController {
 
 	DateTimeFormatter parser = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private final String frmUsuario = "form/frmCadUsuario";
-	
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	//@Autowired
-	//private BCryptPasswordEncoder pe;
-	
-	//@Autowired
-   // private LogService logService;
-	
 
-	//@Autowired
-	//AuthenticationFacade session;
+
 
 	@GetMapping
 	public ModelAndView findAll() {
 		ModelAndView mv = new ModelAndView("consultas/usuarios");
 		mv.addObject("usuarios", usuarioService.findAll());
-		mv.addObject("activePage", "mnuCandidato"); 
+		mv.addObject("activePage", "mnuCandidato");
 		return mv;
 	}
 
 	@GetMapping("/new")
 	public String frmCadastrar(Model model, @ModelAttribute("usuario") Usuario usuario) {
-		
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("activePage", "mnuCandidato");		
-		
-		
+		model.addAttribute("activePage", "mnuCandidato");
 		return "form/frmCadUsuario";
-	}
-	
-	//@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-	@PostMapping("/save")
-	public String saveObject(@ModelAttribute("usuario") Usuario usuario, BindingResult result) {
-		try {
-			
-		
-		
-			
-			//u.setEmail(usuario.getEmail());
-			//u.setSenha(pe.encode(usuario.getSenha()));
-			usuario.setAtivo("1");
-			usuario.setPerfilId(1);
-			
-			usuarioService.save(usuario);
-			
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-	
-		return "redirect:/usuarios";
 	}
 
 	@GetMapping("/edit/{id}")
 	public ModelAndView frmEditar(@PathVariable(name = "id") Integer id) {
-		ModelAndView model = new ModelAndView("form/frmCandidato");
-		Usuario user = usuarioService.findById(id).get();		
-		model.addObject("candidato", user);		
+		ModelAndView model = new ModelAndView(frmUsuario);
+		Usuario user = usuarioService.findById(id).get();
+		model.addObject("candidato", user);
 		return model;
 	}
+
+	@PostMapping("/save")
+	public String saveObject(@ModelAttribute("usuario") Usuario usuario, BindingResult result) {
+		try {			
+			usuario.setPerfilId(1);
+			usuario.setDatacriacao(LocalDateTime.now());
+			usuario.setUltimoacesso(LocalDateTime.now());
+			usuarioService.save(usuario);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/usuarios";
+	}
+
 }
