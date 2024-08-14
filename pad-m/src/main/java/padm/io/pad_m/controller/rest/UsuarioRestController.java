f0d11e8bab13713c46cc22371fa536a58dba7132
034068942259 <estevan.silva@tre-am.jus.br>
2024-08-14 09:01:37 -0400

    result dto

--- pad-m/src/main/java/padm/io/pad_m/controller/rest/UsuarioRestController.java
+++ pad-m/src/main/java/padm/io/pad_m/controller/rest/UsuarioRestController.java
@@ -10,20 +10,15 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import padm.io.pad_m.domain.Usuario;
