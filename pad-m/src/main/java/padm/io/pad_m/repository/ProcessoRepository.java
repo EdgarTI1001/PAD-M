ba7d37f7e25b073e890b996b1adff3461247c843
Estevangomez <gomes.estevan1@gmail.com>
2024-11-04 21:39:21 -0400

    correcao processos

--- pad-m/src/main/java/padm/io/pad_m/repository/ProcessoRepository.java
+++ pad-m/src/main/java/padm/io/pad_m/repository/ProcessoRepository.java
@@ -10,11 +10,10 @@ import padm.io.pad_m.domain.Processo;
 
 public interface ProcessoRepository extends JpaRepository<Processo, Integer> {
 
-	@Query("SELECT p from Processo p WHERE p.setorcriadorId.id =:idSetor AND p.tipo = 'Setor' ORDER BY p.id DESC")
-	List<Processo> findAllBySetor(@Param("idSetor") Integer idSetor);
-	
+	@Query("SELECT p from Processo p WHERE p.setorcriadorId.id =:idSetor ORDER BY p.id DESC")
+	List<Processo> findAllBySetor(@Param("idSetor") Integer idSetor);	
 
-	@Query("SELECT p from Processo p WHERE p.usucriadorId =:idUser AND p.tipo = 'Servidor' ORDER BY p.id DESC")
+	@Query("SELECT p from Processo p WHERE p.usucriadorId =:idUser ORDER BY p.id DESC")
 	List<Processo> findAllByUserCriador(@Param("idUser") Integer idUser);
 	
 	@Query("SELECT p from Processo p WHERE p.tipo.id =:idTipo ORDER BY p.id DESC")
